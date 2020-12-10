081e429ece52310996ba064b87963482c60457ad
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-10 22:14:50 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    @room = Room.find(params[:room_id])
+    @message = Message.new
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_content)
+    if @message.save
+      redirect_to room_messages_path(@room) #indexに戻り、空のroom_idに紐づいた@messageを作り、それらの情報がはいったparamsが↓
+      # フォームから飛ばされるようにredirectする。
+    else
+      render :index
+    end
+
+  end
+
+  private
+  def message_content
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
